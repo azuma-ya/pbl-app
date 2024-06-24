5cc0427b0a5129e5e6612b1bebef2c5fac5c0785
bb4735 <b.kubo5003@gmail.com>
2024-06-24 09:30:25 +0900

    SchoolSettingを実装

--- components/school/SchoolSetting.tsx
+++ components/school/SchoolSetting.tsx
@@ -1,5 +1,26 @@
+import Box from '@mui/material/Button';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import * as React from 'react';
+
 const SchoolSetting = () => {
   return <div>SchoolSetting</div>;
+
+  <Box>
+    <Button 
+      variant="contained"
+      sx={{ background: "#003B46", textAlign: "center", padding: "5px,50%" }}  
+    >
+      合言葉の設定
+    </Button>
+
+    <Button 
+    variant="contained"
+    sx={{ background: "#07575B", textAlign: "center", padding: "5px,50%" }}
+    >
+      役職の割り当て
+    </Button>
+  </Box>
 };
 
 export default SchoolSetting;
