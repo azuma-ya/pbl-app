dd8fcde623b6bde4b1d4372bc8a404d9b4da094a
azuma-ya <kazu.bluemountain@gmail.com>
2024-07-01 09:14:40 +0900

    fix

--- components/school/Position.tsx
+++ components/school/Position.tsx
@@ -4,7 +4,8 @@ import Checkbox from "@mui/material/Checkbox";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
+import type { SelectChangeEvent } from "@mui/material/Select";
+import Select from "@mui/material/Select";
 import * as React from "react";
 
 const Position = () => {
@@ -15,7 +16,7 @@ const Position = () => {
   };
   return (
     <Box>
-      //"Age"でなく"教員名"にするには？ labelを変更？
+      {/* //"Age"でなく"教員名"にするには？ labelを変更？ //教員名の選択 */}
       <Box sx={{ minWidth: 120, textAlign: "center" }}>
         <FormControl fullWidth>
           <InputLabel id="demo-simple-select-label">Age</InputLabel>
@@ -26,14 +27,14 @@ const Position = () => {
             label="Age"
             onChange={handleChange}
           >
-            //valueとかは何にしたらよいか？
+            {/* //valueとかは何にしたらよいか？ */}
             <MenuItem value={10}>〇〇先生</MenuItem>
             <MenuItem value={10}>〇〇先生</MenuItem>
             <MenuItem value={10}>〇〇先生</MenuItem>
           </Select>
         </FormControl>
       </Box>
-      //役職の選択
+      {/* //役職の選択 */}
       <Box sx={{ minWidth: 120, textAlign: "center" }}>
         <FormControl fullWidth>
           <InputLabel id="demo-simple-select-label">Age</InputLabel>
@@ -50,11 +51,11 @@ const Position = () => {
           </Select>
         </FormControl>
       </Box>
-
+      {/* //チェックボックス ??? */}
       <Checkbox />
       <Button
         variant="contained"
-        sx={{ background: "#003B46", textAlign: "center", padding: "5px,50%" }}
+        sx={{ background: "#2f4f4f", textAlign: "center", padding: "5px,50%" }}
       >
         決定
       </Button>
