770ebf7208df2ea29979420dd698d1ea8bf96489
bb4735 <b.kubo5003@gmail.com>
2024-07-01 10:02:23 +0900

    ボタンの位置調整

--- components/school/SchoolSetting.tsx
+++ components/school/SchoolSetting.tsx
@@ -5,14 +5,14 @@ const SchoolSetting = () => {
     <Box>
       <Button
         variant="contained"
-        sx={{ background: "#003B46", textAlign: "center", padding: "5px,50%" }}
+        sx={{ background: "#003B46", textAlign: "center", padding: "20px", margin: "5%"}}
       >
         合言葉の設定
       </Button>
-
+    <br></br>
       <Button
         variant="contained"
-        sx={{ background: "#07575B", textAlign: "center", padding: "5px,50%" }}
+        sx={{ background: "#07575B", textAlign: "center", padding: "20px", margin: "5%"}}
       >
         役職の割り当て
       </Button>
