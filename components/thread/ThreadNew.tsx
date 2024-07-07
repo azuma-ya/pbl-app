bbfcf2f79560ca8f343b27906a581f206582f7e6
Caassimolar <j2200025@gunma-u.ac.jp>
2024-07-08 01:30:26 +0900

    スレッド作成ページのレイアウトを作成

--- components/thread/ThreadNew.tsx
+++ components/thread/ThreadNew.tsx
@@ -1,5 +1,38 @@
+import { Box, Button, TextField, Typography } from "@mui/material";
+
 const ThreadNew = () => {
-  return <div>ThreadNew</div>;
+  return(
+  <Box sx={{
+    width:"100%",
+    height:"75vh",
+    display:"flex",
+    alignItems:"center",
+    justifyContent:"center",
+    flexDirection: 'column',
+  }}>
+    <Typography component="h1" variant="h5">
+      スレッド作成
+    </Typography>
+      　
+    <TextField fullWidth id="outlined-basic" label="タイトル" variant="outlined" />
+      　
+    <TextField fullWidth multiline rows={10} id="outlined-basic" label="詳細" variant="outlined" />
+    <Button
+      variant="contained"
+      sx={{ mt: 3, mb: 2 }}
+    >
+    教師選択
+    </Button>
+    <Button
+      fullWidth
+      variant="contained"
+      sx={{ mt: 3, mb: 2 }}
+    >
+    作成
+    </Button>
+  </Box>
+  )
+  //return <div>ThreadNew</div>;
 };
 
 export default ThreadNew;
