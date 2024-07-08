aa2e38d7e8f92778918d7d014ace418f1e595ab8
azuma-ya <kazu.bluemountain@gmail.com>
2024-07-09 03:22:38 +0900

    リアルタイム通信を実装する

--- app/api/comment/route.ts
+++ app/api/comment/route.ts
@@ -3,6 +3,8 @@ import prisma from "@/lib/prisma";
 import { getPusherInstance } from "@/lib/pusher/server";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 const pusherServer = getPusherInstance();
 
 export const POST = async (req: Request) => {
