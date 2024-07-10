d59b3126acbb4054255b2ad39c90683f77824b4c
azuma-ya <kazu.bluemountain@gmail.com>
2024-07-10 14:09:03 +0900

    change max duration 2

--- app/api/trpc/[trpc]/route.ts
+++ app/api/trpc/[trpc]/route.ts
@@ -4,7 +4,11 @@ import { appRouter } from "@/trpc/server";
 
 export const dynamic = "force-dynamic";
 
-export const maxDuration = 60;
+export const maxDuration = 59;
+
+export const config = {
+  maxDuration: 59,
+};
 
 const handlers = async (req: Request) => {
   const response = await fetchRequestHandler({
